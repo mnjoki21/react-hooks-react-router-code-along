7b849c1a62b5fc5f5a9339238eacf8eb78622862
mitchelle <mitchellenjoki7@gmail.com>
2022-07-22 23:05:29 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,106 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-
-const linkStyles = {
-  display: "inline-block",
-  width: "50px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
-};
-
-function NavBar() {
-  return (
-    <div>
-      <NavLink
-        to="/"
-        exact 
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/login"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Login
-      </NavLink>
-    </div>
-  );
-}
-
-
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
 ReactDOM.render(
   <BrowserRouter>
-  <NavBar />
-  <Switch>
-    <Route path="/">
-      <Home />
-    </Route>
-    <Route path="/about">
-      <About />
-    </Route>
-    <Route path ="/login">
-      <Login />
-    </Route>
-    </Switch>
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
